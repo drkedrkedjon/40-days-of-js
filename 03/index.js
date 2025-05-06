cc26faee8b2d1bf22eba776f1377dc44ce0a4005
Sasha S.P <sasa@bubulazy.com>
2025-05-06 20:26:34 +0200

    Day 4

--- 03/index.js
+++ 03/index.js
@@ -1,1 +1,35 @@
-console.log("Jujuuuu");
+console.log("Day 03");
+
+console.log(23 % 4);
+
+console.log(true && false && "Sasa" && "888");
+console.log((true && false) || ("Sasa" && "888") || false);
+
+const odd = 8 % 3 === 0 ? "pair" : "odd";
+console.log(odd);
+
+const age = 35;
+
+console.log(age >= 18);
+
+console.log((12300 + (12300 / 100) * 20) * 12);
+
+const color = "green";
+console.log(color === "red" ? "STOP" : "GO");
+
+const units = 35;
+const year = units * 150 * 12;
+const discYear = year - (year / 100) * 20;
+console.log(discYear);
+
+const year1 = 2400;
+const leap1 = (year1 % 4 === 0 && year1 % 100 !== 0) || year1 % 400 === 0;
+console.log(leap1);
+
+const a = 20;
+const b = 16;
+const c = 25;
+
+const sasa = a > b ? (a > c ? a : c) : b > c ? b : c;
+
+console.log(sasa);
