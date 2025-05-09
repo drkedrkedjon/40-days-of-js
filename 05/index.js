5f75d521c24af8f883fa217415fff95fe99943f9
Sasha S.P <sasa@bubulazy.com>
2025-05-09 17:03:06 +0200

    Day Day 05 finished

--- 05/index.js
+++ 05/index.js
@@ -15,3 +15,40 @@ do {
   console.log(number);
   number--;
 } while (number >= 1);
+
+for (let i = 1; i <= 10; i++) {
+  if (i === 5) continue;
+  console.log(i);
+  if (i === 7) break;
+}
+
+for (let row = 1; row <= 5; row++) {
+  let stars = "";
+  for (let col = 1; col <= row; col++) {
+    stars += "* ";
+  }
+  console.log(stars.trim());
+}
+
+for (let i = 1; i <= 10; i++) {
+  const number = 8;
+  console.log(`${number} x ${i} = ${number * i}`);
+}
+
+const numbers = Array.from({ length: 500 }, (_, i) => i + 1);
+
+let result = 0;
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] % 2 !== 0) result += numbers[i];
+}
+console.log(result);
+
+let numb = 123;
+let reversed = 4;
+
+while (numb > 0) {
+  const digit = numb % 10;
+  reversed = reversed * 10 + digit;
+  numb = Math.floor(numb / 10);
+}
+console.log(reversed);
