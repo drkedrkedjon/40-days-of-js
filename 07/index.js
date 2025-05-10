be2e449acb7376c09cb9891b4a67aded830068bd
Sasha S.P <sasa@bubulazy.com>
2025-05-10 18:55:25 +0200

    Day 08

--- 07/index.js
+++ 07/index.js
@@ -1,31 +1,79 @@
-console.log("Day 07");
+// console.log("Day 07");
+
+// function game() {
+//   const userChoice = prompt("rock, paper or scissors");
+
+//   const options = ["rock", "paper", "scissors"];
+//   let randomNumber = Math.floor(Math.random() * 3);
+//   const computerChoice = options[randomNumber];
+
+//   if (userChoice === computerChoice) alert("No winner, play again");
+
+//   if (userChoice === "rock") {
+//     if (computerChoice === "paper") alert("Computer wins");
+//     else if (computerChoice === "scissors") alert("Player wins");
+//   }
+
+//   if (userChoice === "paper") {
+//     if (computerChoice === "rock") alert("Player wins");
+//     else if (computerChoice === "scissors") alert("Computer wins");
+//   }
+
+//   if (userChoice === "scissors") {
+//     if (computerChoice === "paper") alert("Computer wins");
+//     else if (computerChoice === "rock") alert("Player wins");
+//   }
+
+//   const newGame = prompt("Want to play again");
+//   if (newGame === "y") game();
+// }
+
+// game();
+
+// random number to 10
+// prompt user to enter number
+// keep track of number of atempt
+// compare both numbers and prompt again the user for next one
+// keep on going until user hits
+// Alert user how did he worked.
+
+let computerNumberChoice = Math.floor(Math.random() * 10) + 1;
+console.log(computerNumberChoice);
+let userChoiceString = prompt("Enter a number from 1 to 10");
+let userChoice = "";
+let numberOfAttempts = 0;
+
+function tooSmall() {
+  userChoiceString = prompt(
+    "Enter new number, your choice number is too SMALL"
+  );
+  userChoiceString && game();
+}
+
+function tooBig() {
+  userChoiceString = prompt("Enter new number, your choice number is too BIG");
+  userChoiceString && game();
+}
 
 function game() {
-  const userChoice = prompt("rock, paper or scissors");
+  console.log(computerNumberChoice);
+  userChoice = Number(userChoiceString);
+  numberOfAttempts++;
 
-  const options = ["rock", "paper", "scissors"];
-  let randomNumber = Math.floor(Math.random() * 3);
-  const computerChoice = options[randomNumber];
-
-  if (userChoice === computerChoice) alert("No winner, play again");
-
-  if (userChoice === "rock") {
-    if (computerChoice === "paper") alert("Computer wins");
-    else if (computerChoice === "scissors") alert("Player wins");
+  if (computerNumberChoice === userChoice) {
+    alert(`You have guessed the corect NUMBER in ${numberOfAttempts} attempts`);
+    computerNumberChoice = Math.floor(Math.random() * 10) + 1;
+    userChoiceString = prompt("Enter a number from 1 to 10");
+    userChoice = Number(userChoiceString);
+    numberOfAttempts = 0;
   }
 
-  if (userChoice === "paper") {
-    if (computerChoice === "rock") alert("Player wins");
-    else if (computerChoice === "scissors") alert("Computer wins");
+  if (computerNumberChoice > userChoice) {
+    tooSmall();
   }
-
-  if (userChoice === "scissors") {
-    if (computerChoice === "paper") alert("Computer wins");
-    else if (computerChoice === "rock") alert("Player wins");
+  if (computerNumberChoice < userChoice) {
+    tooBig();
   }
-
-  const newGame = prompt("Want to play again");
-  if (newGame === "y") game();
 }
 
 game();
---
+++ 08/index.js
@@ -0,0 +1,5 @@
+console.log("Day 08");
+
+// Lexicon Context es donde esta mi codigo escrito en el documento .js cargado en navegador
+// Global Execution Context es entorno creado por motor de JS. Incluyendo si no hay ni una linea de codigo escrito de JS, motor de JS crea window con sus propiedades y metodos() y tambien el contexto THIS.
+// THIS context es WINDOW (this === window => true) no porque son del mismo type sino porque apuntan a mismo registro en la memoria.
