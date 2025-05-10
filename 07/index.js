f0a6a339ddb7e42910dde1170b6b53e7efcbdccf
Sasha S.P <sasa@bubulazy.com>
2025-05-10 13:59:20 +0200

    Day Day 07 time to do proyect

--- 07/index.js
+++ 07/index.js
@@ -1,1 +1,31 @@
 console.log("Day 07");
+
+function game() {
+  const userChoice = prompt("rock, paper or scissors");
+
+  const options = ["rock", "paper", "scissors"];
+  let randomNumber = Math.floor(Math.random() * 3);
+  const computerChoice = options[randomNumber];
+
+  if (userChoice === computerChoice) alert("No winner, play again");
+
+  if (userChoice === "rock") {
+    if (computerChoice === "paper") alert("Computer wins");
+    else if (computerChoice === "scissors") alert("Player wins");
+  }
+
+  if (userChoice === "paper") {
+    if (computerChoice === "rock") alert("Player wins");
+    else if (computerChoice === "scissors") alert("Computer wins");
+  }
+
+  if (userChoice === "scissors") {
+    if (computerChoice === "paper") alert("Computer wins");
+    else if (computerChoice === "rock") alert("Player wins");
+  }
+
+  const newGame = prompt("Want to play again");
+  if (newGame === "y") game();
+}
+
+game();
