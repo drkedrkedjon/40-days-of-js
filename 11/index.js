17e5d28e5bdf64f0444469d9ee9e74f77df3567d
Sasha S.P <sasa@bubulazy.com>
2025-05-12 11:25:43 +0200

    Day 12

--- 11/index.js
+++ 11/index.js
@@ -1,1 +1,42 @@
-console.log("Day 10");
+console.log("Day 11");
+
+// CLOSURES
+
+function outer(input) {
+  let numb = input;
+
+  return function inner(input) {
+    console.log(numb++ * input);
+  };
+}
+
+const inner = outer(5);
+inner(5);
+inner(5);
+
+function bank(initialValue) {
+  balance = initialValue;
+
+  return {
+    status: () => {
+      return balance;
+    },
+    deposit: (deposit) => {
+      balance += deposit;
+      return balance;
+    },
+    draw: (deposit) => {
+      balance -= deposit;
+      return balance;
+    },
+  };
+}
+
+const myAccount = bank(1000);
+
+console.log(myAccount.status());
+console.log(myAccount.deposit(400));
+console.log(myAccount.draw(400));
+console.log(myAccount.draw(400));
+console.log(myAccount.draw(400));
+console.log(myAccount.draw(400));
