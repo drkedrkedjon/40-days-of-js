ff3eb4e3260a918abc849fa218ecad4a5c0fe018
Sasha S.P <sasa@bubulazy.com>
2025-06-04 13:42:51 +0200

    Day 17 start

--- 17/index.js
+++ 17/index.js
@@ -1,1 +1,12 @@
 "use strict";
+console.log(document);
+
+const idElem = document.querySelector("#subheading");
+console.log(idElem);
+
+const colection = document.querySelectorAll("h1.heading");
+console.log(colection);
+colection.forEach((elem) => console.log(elem));
+
+const titelElem = document.querySelector("h2#subheading");
+console.log(titelElem);
