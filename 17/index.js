42c6803a1432017a8e9c1810ac1b1f93a86c989a
Sasha S.P <sasa@bubulazy.com>
2025-06-04 14:19:46 +0200

    Day 18 start

--- 17/index.js
+++ 17/index.js
@@ -1,12 +1,28 @@
 "use strict";
-console.log(document);
+// console.log(document);
 
-const idElem = document.querySelector("#subheading");
-console.log(idElem);
+// const idElem = document.querySelector("#subheading");
+// console.log(idElem);
 
-const colection = document.querySelectorAll("h1.heading");
-console.log(colection);
-colection.forEach((elem) => console.log(elem));
+// const colection = document.querySelectorAll("h1.heading");
+// console.log(colection);
+// colection.forEach((elem) => console.log(elem));
 
-const titelElem = document.querySelector("h2#subheading");
-console.log(titelElem);
+// const titelElem = document.querySelector("h2#subheading");
+// console.log(titelElem);
+
+const input = document.querySelector("#inputField");
+const listItems = document.querySelectorAll("ul.list li");
+
+function searchFunction() {
+  let queryString = input.value;
+
+  listItems.forEach(
+    (item) =>
+      (item.style.display = item.textContent
+        .toLowerCase()
+        .includes(queryString.toLowerCase())
+        ? ""
+        : "none")
+  );
+}
