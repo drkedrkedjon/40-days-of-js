57de3954bd020bad454df449fae7789e499c7cfc
Sasha S.P <sasa@bubulazy.com>
2025-06-05 09:40:59 +0200

    Day 19 - 1

--- 19/index.js
+++ 19/index.js
@@ -1,1 +1,11 @@
 "use strict";
+let counter = 0;
+function handleClick() {
+  counter++;
+  console.log(counter);
+}
+
+const btn1 = document.querySelector("#btn1");
+btn1.addEventListener("click", handleClick);
+// btn1.removeEventListener("click", handleClick);
+// One can add as many events onClick as he wants
