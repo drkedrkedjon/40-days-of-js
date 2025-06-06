fcbc92e285aa10aec430fa9f0f50fa451cd621b7
Sasha S.P <sasa@bubulazy.com>
2025-06-06 14:01:37 +0200

    Day 20 - 3

--- 20/index.js
+++ 20/index.js
@@ -26,3 +26,40 @@ for (let i = 0; i <= 3; i++) {
 const lista = document.querySelector("#my-list").appendChild(fragment);
 
 // Range es para seleccionar un range de por ejemplo characters in a string sentence. No se, no me molesto con eso ahora...
+
+// Mutation Observer
+// It observes some changes on specific element and then it does what ever you want to do...
+
+const target = document.getElementById("watchMe");
+
+const observer = new MutationObserver((mutationsList, observer) => {
+  for (const mutation of mutationsList) {
+    console.log(`Type of mutation: ${mutation.type}`);
+
+    if (mutation.type === "childList") {
+      console.log("A child node was added or removed.");
+    }
+
+    if (mutation.type === "attributes") {
+      console.log(`Attribute ${mutation.attributeName} was changed.`);
+    }
+
+    if (mutation.type === "characterData") {
+      console.log(`Text content changed to: ${mutation.target.data}`);
+    }
+  }
+});
+
+const config = {
+  subtree: true,
+  characterData: true,
+  childList: true,
+  attributes: true,
+};
+
+observer.observe(target, config);
+
+function changeDOM() {
+  target.textContent = "Goodbye!";
+  target.setAttribute("data-status", "Changed");
+}
