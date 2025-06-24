72d47307c7456adf899f2b9d2b341379d48059c6
Sasha S.P <sasa@bubulazy.com>
2025-06-24 13:44:46 +0200

    Day 26 - 2

--- 26/index.js
+++ 26/index.js
@@ -1,1 +1,24 @@
 "use strict";
+
+// Never call promisas inside the LOOP - use Promise.allSettled()
+const idSet = ["1", "2", "3", "500"];
+
+async function fetcher() {
+  const response = await Promise.allSettled(
+    idSet.map((id) => fetch(`https://jsonplaceholder.typicode.com/users/${id}`))
+  );
+  const data = await Promise.allSettled(
+    response.map((res) => res.value.json())
+  );
+
+  data.forEach((user) => {
+    const person = user.value;
+    console.log(`
+      ${person.name}
+      ${person.email}
+    
+      `);
+  });
+}
+
+// fetcher();
