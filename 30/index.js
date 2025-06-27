ed97ad74341d91c562cfde03ea0c5700f4a1b830
Sasha S.P <sasa@bubulazy.com>
2025-06-27 09:50:12 +0200

    Day 30 - 2

--- 30/index.js
+++ 30/index.js
@@ -1,1 +1,17 @@
 "use strict";
+
+class Car {
+  constructor(brand, model) {
+    (this.brand = brand), (this.model = model);
+  }
+
+  printModel() {
+    console.log(`This ${this.brand}, this ${this.model}`, this);
+  }
+}
+
+const one = new Car("Fiat", "500");
+const two = new Car("Peugi", "e-208");
+
+one.printModel();
+two.printModel();
