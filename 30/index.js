004db1a9de8773a2976b0d111d138e707ed245b5
Sasha S.P <sasa@bubulazy.com>
2025-06-27 10:29:34 +0200

    Day 30 - 3

--- 30/index.js
+++ 30/index.js
@@ -15,3 +15,55 @@ const two = new Car("Peugi", "e-208");
 
 one.printModel();
 two.printModel();
+
+console.log(typeof Car); // CLass constructor is a function. typeOf returns : Function.
+
+// Class as expresion
+
+const Trains = class {
+  // rest is the same, but keep Capital first leter for conviniance
+};
+
+// Named Class
+const Busses = class CreateFuckaBuses {
+  // whatever
+}; // One call this shit by invocing variable: const busLine12 = new Busses
+
+//
+
+class Cars {
+  _type = "Vehicle";
+  constructor(brand, model) {
+    this.brand = brand;
+    this.model = model;
+  }
+  // The goot shit about get & set is that we can do some validations in seter or extra procesing in geter, depending of bussines model.
+  get carType() {
+    return `Your car type is:  ${this._type}`;
+  }
+
+  set carType(newType) {
+    if (newType) {
+      this._type = newType;
+    } else {
+      console.warn("Shit warning");
+    }
+  }
+
+  printModel() {
+    console.log(
+      `This ${this.brand}, this ${this.model} and of ${this._type} type`
+    );
+  }
+}
+
+const ones = new Cars("Fiat", "500");
+const twos = new Cars("Peugi", "e-208");
+
+console.log(ones.carType);
+ones.carType = "Banana";
+
+ones.printModel();
+twos.printModel();
+
+//
