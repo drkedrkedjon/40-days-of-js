2e8bfa7c1c5c745c90437e46a205bce25467da09
Sasha S.P. <sasa@bubulazy.com>
2025-07-03 20:06:47 +0100

    32 - 3

--- 32/index.js
+++ 32/index.js
@@ -3,3 +3,15 @@ import { sum as sum1, sub, mul } from "./calc.js";
 import division from "./calc.js";
 
 console.log(sum1(3, 5), sub(5, 8), mul(5, 6), division(55, 2));
+
+// Namespace import ALL
+import * as calc from "./calc.js";
+console.log(calc.sum(3, 5), calc.sub(5, 8), calc.mul(5, 6));
+
+// Import modules dinamicamente
+if (true) {
+  const { sub } = await import("./calc.js");
+  console.log(sub(4, 9));
+}
+
+// Se pueden importar usando Promises.all([ import("./calc.js"), import("./palc.js");  ])
